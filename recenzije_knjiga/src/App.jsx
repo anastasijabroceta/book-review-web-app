f4f8ea1ed444c6d641c03e2bb2d288f12d1e80ce
srnabatinic <srnabatinic007@gmail.com>
2026-04-21 18:12:19 +0200

    added routing and navbar links

--- recenzije_knjiga/src/App.jsx
+++ recenzije_knjiga/src/App.jsx
@@ -1,20 +1,33 @@
 import { Button } from 'bootstrap';
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import RecommendedBooks from './components/RecommendedBooks';
 import FamousBooks from './components/FamousBooks';
+import AuthorList from './components/AuthorList';
 
 function App() {
   return (
-    <div className='App'>
-      <header id='header'>
-        <Header/>
-        <HeroSection/>
-      </header>
-      <RecommendedBooks/>
-      <FamousBooks/>
-    </div>
+    <Router>
+      <div className='App'>
+        <header id='header'>
+          <Header />
+        </header>
+
+        <Routes>
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <RecommendedBooks />
+              <FamousBooks />
+            </>
+          } />
+
+          <Route path="/authors" element={<AuthorList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

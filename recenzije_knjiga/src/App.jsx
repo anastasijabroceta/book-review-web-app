f27007df9262075981c76e9a5be4944d8a03e583
anastasijabroceta <anastasija.broceta@gmail.com>
2026-04-21 18:12:04 +0200

    footer-section

--- recenzije_knjiga/src/App.jsx
+++ recenzije_knjiga/src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import RecommendedBooks from './components/RecommendedBooks';
 import FamousBooks from './components/FamousBooks';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <div className='App'>
@@ -14,6 +14,7 @@ function App() {
       </header>
       <RecommendedBooks/>
       <FamousBooks/>
+      <Footer/>
     </div>
   );
 }
